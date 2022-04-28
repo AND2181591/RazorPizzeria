b9ee298dc6dfce5cbd0e269f8ee800a0bc450d16
Andrew <AND2181591@maricopa.edu>
2022-04-27 22:55:44 -0700

    Completed tutorial at lesson 51

---
+++ Migrations/20220427231654_addedPizzaOrderCorrection.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPizzeria.Migrations
+{
+    public partial class addedPizzaOrderCorrection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty");
+
+            migrationBuilder.RenameTable(
+                name: "MyProperty",
+                newName: "PizzaOrders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PizzaOrders",
+                table: "PizzaOrders",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PizzaOrders",
+                table: "PizzaOrders");
+
+            migrationBuilder.RenameTable(
+                name: "PizzaOrders",
+                newName: "MyProperty");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty",
+                column: "Id");
+        }
+    }
+}
